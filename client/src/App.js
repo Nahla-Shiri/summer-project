adfbeb895c2f72f1002195c7fa2f6f1cca0cb815
Nahla Shiri <nahla.job@gmail.com>
2020-06-26 10:58:57 +0100

    setup react + router exemple

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import {Home, Login} from './pages';
+import { NavBar } from './components';
 
 function App() {
   return (
     <div>
-      <h1>Hello react</h1>
+      <NavBar />
+      <Route path="/" component={Home} exact />
+      <Route path="/login" component={Login} exact />
     </div>
   );
 }
