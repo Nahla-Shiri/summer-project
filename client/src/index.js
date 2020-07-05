7f53680990627093344e8501c4fd8950e1d3af8f
Nahla Shiri <nahla.job@gmail.com>
2020-07-05 18:16:26 +0100

    add sass  boileplate to front

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import {BrowserRouter} from 'react-router-dom';
 import {onLoadingSignIn} from './actions/auth_actions'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/sass/style.scss';
 
 
 import App from './App';
